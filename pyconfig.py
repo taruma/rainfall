9457e3838cdd58fa662c554a99ee9caf79543f43
Taruma Sakti <tarumainfo@gmail.com>
2022-05-08 20:03:50 +0700

    🤭 forgot to disable theme

--- pyconfig.py
+++ pyconfig.py
@@ -2,4 +2,4 @@ from box import Box
 
 _CONFIG_PATH = "app_config.yml"
 appConfig = Box.from_yaml(filename=_CONFIG_PATH)
-appConfig.DASH_THEME.THEME = "SKETCHY"
+# appConfig.DASH_THEME.THEME = "SKETCHY"
